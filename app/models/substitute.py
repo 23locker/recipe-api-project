c471a9bf329e8d6bacfb0e74dd1df49776f3cd77
tt <1tokatoma1@gmail.com>
2025-12-21 21:21:38 +0300

    create models for project(user, ingridient, substitute, category)

---
+++ app/models/substitute.py
@@ -0,0 +1,22 @@
+from pydoc import describe
+
+from tortoise import Model, fields
+
+
+class Subtitute(Model):
+    id = fields.IntField(pk=True)
+    original_ingridient: fields.ForeignKeyRelation["Ingridient"] = (
+        fields.ForeignKeyField(
+            "models.Ingridient",
+            related_name="subtitues",
+        )
+    )
+    subtitutes_ingridient: fields.ForeignKeyRelation["Ingridient"] = (
+        fields.ForeignKeyField("models.ingridient")
+    )
+    coefficient = fields.FloatField(description="Коэффициент замены от 0 до 3")
+    created_at = fields.DatetimeField(auto_now_add=True)
+
+    class Meta:
+        table = "subtitutes"
+        unique_together = (("original_ingridient", "subtitute_ingridient"),)
