ed05b1965077302c55d824566277c75d79afea03
tt <1tokatoma1@gmail.com>
2025-12-22 16:41:08 +0300

    do small fixes with some grammatical errors

--- app/models/tortoise/substitute.py
+++ app/models/tortoise/substitute.py
@@ -5,13 +5,13 @@ from tortoise import Model, fields
 
 class Subtitute(Model):
     id = fields.IntField(pk=True)
-    original_ingridient: fields.ForeignKeyRelation["Ingridient"] = (
+    original_ingridient: fields.ForeignKeyRelation["Ingredient"] = (
         fields.ForeignKeyField(
             "models.Ingridient",
             related_name="subtitues",
         )
     )
-    subtitutes_ingridient: fields.ForeignKeyRelation["Ingridient"] = (
+    subtitutes_ingridient_id: fields.ForeignKeyRelation["Ingredient"] = (
         fields.ForeignKeyField("models.ingridient")
     )
     coefficient = fields.FloatField(description="Коэффициент замены от 0 до 3")
--- app/utils/security.py
+++ app/utils/security.py
@@ -22,7 +22,7 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
 
 
-def access_token(data: Dict, expires_delta: Optional[timedelta] = None) -> str:
+def create_access_token(data: Dict, expires_delta: Optional[timedelta] = None) -> str:
     to_encode = data.copy()
 
     if expires_delta:
